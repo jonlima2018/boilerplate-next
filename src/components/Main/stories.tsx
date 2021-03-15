08de3012388a40cd52a5e1c9d50eb6a821ffc9f8
jonlima <jonathan_martins_lima@yahoo.com.br>
2021-03-15 12:24:13 -0300

    config boilerplate

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,13 @@
+import Main from '.'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
